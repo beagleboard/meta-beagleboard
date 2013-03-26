984855bc07645bb1f3eb67d0e18d0adea7418463
Koen Kooi <koen@dominion.thruhere.net>
2013-03-26 14:45:40 +0100

    contrib: beaglebone eMMC flashing scripts
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

---
+++ contrib/bone-flash-tool/emmc.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# (c) Copyright 2013 Koen Kooi <koen@dominion.thruhere.net>
+# Licensed under terms of GPLv2
+
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin
+
+echo "Paritioning eMMC"
+./mkcard.sh /dev/mmcblk1
+
+echo "Mounting partitions"
+mkdir -p /media/1
+mkdir -p /media/2
+mount /dev/mmcblk1p1 /media/1 -o async
+mount /dev/mmcblk1p2 /media/2 -o async,noatime
+
+echo "Copying bootloader files"
+cp MLO u-boot.img /media/1
+echo "optargs=quiet" > /media/1/uEnv.txt
+
+sync
+
+echo "Extracting rootfs"
+tar zxf Angstrom-Cloud9-IDE-GNOME-eglibc-ipk-v2012.12-beaglebone.rootfs.tar.gz -C /media/2
+
+echo "Populating VFAT partition"
+if [ -d /media/2/usr/share/beaglebone-getting-started ] ; then
+	cp -r /media/2/usr/share/beaglebone-getting-started/* /media/1
+fi
+
+umount /media/1
+
+echo "Generating machine ID"
+systemd-nspawn -D /media/2 /bin/systemd-machine-id-setup
+
+echo "Running Postinsts"
+cpufreq-set -g performance
+systemd-nspawn -D /media/2 /usr/bin/opkg-cl configure
+cpufreq-set -g ondemand
+
+#echo "Setting timezone to Europe/Paris"
+#systemd-nspawn -D /media/2 /usr/bin/timedatectl set-timezone Europe/Paris
+
+rm /media/2/etc/pam.d/gdm-autologin
+
+umount /media/2
+
+echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
+echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
+echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger
+echo default-on > /sys/class/leds/beaglebone\:green\:usr3/trigger
+
+
---
+++ contrib/bone-flash-tool/mkcard.sh
@@ -0,0 +1,80 @@
+#! /bin/sh
+# mkcard.sh v0.5
+# (c) Copyright 2009 Graeme Gregory <dp@xora.org.uk>
+# (c) Copyright 2013 Koen Kooi <koen@dominion.thruhere.net>
+# Licensed under terms of GPLv2
+#
+# Parts of the procudure base on the work of Denys Dmytriyenko
+# http://wiki.omap.com/index.php/MMC_Boot_Format
+
+export LC_ALL=C
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <drive>"
+	exit 1;
+fi
+
+DRIVE=$1
+
+dd if=/dev/zero of=$DRIVE bs=1M count=1
+
+SIZE=`fdisk -l $DRIVE | grep Disk | grep bytes | awk '{print $5}'`
+
+echo DISK SIZE - $SIZE bytes
+
+CYLINDERS=`echo $SIZE/255/63/512 | bc`
+
+echo CYLINDERS - $CYLINDERS
+
+{
+echo ,9,0x0C,*
+echo ,,,-
+} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
+
+sleep 1
+
+
+if [ -x `which kpartx` ]; then
+	kpartx -a ${DRIVE}
+fi
+
+# handle various device names.
+# note something like fdisk -l /dev/loop0 | egrep -E '^/dev' | cut -d' ' -f1 
+# won't work due to https://bugzilla.redhat.com/show_bug.cgi?id=649572
+
+PARTITION1=${DRIVE}1
+if [ ! -b ${PARTITION1} ]; then
+	PARTITION1=${DRIVE}p1
+fi
+
+DRIVE_NAME=`basename $DRIVE`
+DEV_DIR=`dirname $DRIVE`
+
+if [ ! -b ${PARTITION1} ]; then
+	PARTITION1=$DEV_DIR/mapper/${DRIVE_NAME}p1
+fi
+
+PARTITION2=${DRIVE}2
+if [ ! -b ${PARTITION2} ]; then
+	PARTITION2=${DRIVE}p2
+fi
+if [ ! -b ${PARTITION2} ]; then
+	PARTITION2=$DEV_DIR/mapper/${DRIVE_NAME}p2
+fi
+
+
+# now make partitions.
+if [ -b ${PARTITION1} ]; then
+	umount ${PARTITION1}
+	mkfs.vfat -F 32 -n "BEAGLEBONE" ${PARTITION1}
+else
+	echo "Cant find boot partition in /dev"
+fi
+
+if [ -b ${PARITION2} ]; then
+	umount ${PARTITION2}
+	mkfs.ext4  -L "Angstrom" ${PARTITION2} 
+else
+	echo "Cant find rootfs partition in /dev"
+fi
+
